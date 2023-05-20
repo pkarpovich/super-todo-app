b098342554d87915026e8ae84bb114ddbd36be18
Pavel Karpovich <pavel.karpovich@outlook.com>
2023-05-20 16:59:18 +0200

    refactor(index.html): update title to "Super Todo App" and remove vite.svg refactor(App.tsx): remove unnecessary code and assets, display "Super Todo App"
    The changes update the title of the application to "Super Todo App" and remove the vite.svg file, as it is no longer needed. In App.tsx, unnecessary code and assets related to the previous template are removed, and the application now displays "Super Todo App" as its main content. This simplifies the codebase and focuses on the new purpose of the application.
    
    refactor(assets): remove unused react.svg file
    refactor(global-styles.ts): simplify global styles and remove unnecessary styles
    The unused react.svg file has been removed to keep the assets folder clean. The global styles have been simplified and unnecessary styles have been removed to improve the overall styling and maintainability of the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,7 @@
-import { useState } from "react";
-import { styled } from "@linaria/react";
-
 import { globalStyles } from "./global-styles.ts";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
-const Logo = styled.img`
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-`;
-
-const Card = styled.div`
-  padding: 2em;
-`;
-
-const Docks = styled.p`
-  color: #888;
-`;
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className={globalStyles}>
-      <div>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <Logo src={viteLogo} alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <Logo src={reactLogo} alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <Card>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </Card>
-        <Docks>Click on the Vite and React logos to learn more</Docks>
-      </div>
-    </div>
-  );
+  return <div className={globalStyles}>Super Todo App</div>;
 }
 
 export default App;
